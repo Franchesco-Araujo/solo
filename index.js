b15e96bfd2bf126b84b58843a37d13ade4924a66
Franchesco Araujo <ti.franchescoaraujo@gmail.com>
2021-03-04 16:02:01 -0300

    fazendo chamada para 001 com axios

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer zyoTbfwzShovzqfF1HL6Pp7R9Mc");
-myHeaders.append("Cookie", "BrowserId=o5tw85btEeq_EY96rL1b9w");
+var axios = require('axios');
+var data = '';
 
-var raw = "";
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
+var config = {
+  method: 'get',
+  url: 'https://zzrw-001.sandbox.us01.dx.commercecloud.salesforce.com/s/-/dw/data/v20_4/products/produto-api-tres',
+  headers: { 
+    'Authorization': 'Bearer C2EYbLNRPvD-KOwfHPs_Up-PAXg', 
+    'Cookie': 'BrowserId=o5tw85btEeq_EY96rL1b9w'
+  },
+  data : data
 };
 
-fetch("https://zzrw-001.sandbox.us01.dx.commercecloud.salesforce.com/s/-/dw/data/v20_4/products/produto-api-tres", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+console.log("carregando da 001");
