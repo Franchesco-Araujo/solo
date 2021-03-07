3e1cdfb04371f314eaed54ccd5b8e8f7f55c2e40
Franchesco Araujo <ti.franchescoaraujo@gmail.com>
2021-03-07 17:19:04 -0300

    primeiros passo

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,45 @@
 var btn = document.querySelector('.btn');
 var ul = document.querySelector('ul');
+var avatar = document.querySelector('.foto');
+var nomeUser = document.querySelector('#nome');
+var loginUser = document.querySelector('#login');
+var users = [];
 
-function buscaPerfil() {
+
+
+
+//função click do botão
+function buscaPerfil(event) {
+  event.preventDefault();
 
   var input = document.querySelector('input').value;
-  var li = document.createElement('li');
-  console.log(input, '235');
+
+  //linha provisória para testes
+  input = "Franchesco-Araujo"
+  //armazenando ids em um array
+  console.log(users.push(input));
+
+  console.log(users);
   axios.get(`https://api.github.com/users/${input}`)
 
     .then((resultado) => {
       console.log(resultado, 'dentro');
+      return perfil =
+        [
+          nomeUser.innerHTML = resultado.data.name,
+          loginUser.innerHTML = resultado.data.login,
+          avatar.innerHTML = resultado.data.id
+
+        ]
+
 
     }).catch((erro) => {
       console.log(erro);
       console.log('erro');
     });
 }
+
+
+
 
 btn.onclick = buscaPerfil;
